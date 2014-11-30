fa7d062abc7b9169c51c91e85312d1602c60671e
SAITOU Keita <keita44.f4@gmail.com>
2014-12-01 08:01:13 +0900

    Cosmetic change

--- bash_script/makesmi.sh
+++ bash_script/makesmi.sh
@@ -5,4 +5,4 @@ output=${input%.*}".smi"
 
 smlsharp < ${input} | tail -n +2 | sed 's/^#[ >]*\(.*\)/\1/g' | \
   sed 's/\[//g' | sed 's/\]//g' | sed 's/'\''[a-z]\.//g' |
-  sed 's/= fn //g' | sed 's/= .* :/:/g'> ${output}
+  sed 's/= fn //g' | sed 's/= .* :/:/g' > ${output}
