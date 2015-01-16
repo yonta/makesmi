7e4f8a165804d26a33b039fa1ea3c2af2d0126a3
SAITOU Keita <keita44.f4@gmail.com>
2015-01-17 00:09:22 +0900

    Fix bug, some SML files says multiple hash tokens like "# # >..."

--- bash_script/makesmi.sh
+++ bash_script/makesmi.sh
@@ -3,6 +3,6 @@
 input=$1
 output=${input%.*}".smi"
 
-smlsharp < ${input} | tail -n +2 | sed 's/^#[ >]*\(.*\)/\1/g' | \
+smlsharp < ${input} | tail -n +2 | sed 's/^#[#> ]*\(.*\)/\1/g' | \
   sed 's/\[//g' | sed 's/\]//g' | sed 's/'\''[a-z]\.//g' |
   sed 's/= fn //g' | sed 's/= .* :/:/g' > ${output}
