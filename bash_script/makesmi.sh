a83ad84a671a1244383cc59738a8685f16a070ef
SAITOU Keita <keita44.f4@gmail.com>
2015-01-17 00:12:05 +0900

    Change makesmi.sh, to adapt to multiple depended SML files

--- bash_script/makesmi.sh
+++ bash_script/makesmi.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 
-input=$1
-output=${input%.*}".smi"
+outputname=${1%.sml}".smi"
+tempname=${1%.sml}".tmp"
 
-smlsharp < ${input} | tail -n +2 | sed 's/^#[#> ]*\(.*\)/\1/g' | \
+cat $@ > ${tempname}
+smlsharp < ${tempname} | tail -n +2 | sed 's/^#[#> ]*\(.*\)/\1/g' | \
   sed 's/\[//g' | sed 's/\]//g' | sed 's/'\''[a-z]\.//g' |
-  sed 's/= fn //g' | sed 's/= .* :/:/g' > ${output}
+  sed 's/= fn //g' | sed 's/= .* :/:/g' > ${outputname}
